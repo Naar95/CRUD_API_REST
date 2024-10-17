9e51e258bfafe389e9e9bb9faa2be04afde32929
Nahuel Ivan <arellano.nahuel95@gmail.com>
2024-10-17 11:45:06 -0300

    Inicio de commit: se prepara el repositorio REST WebApi hecho en lenguanje C# para el subir a github

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            var cors = new EnableCorsAttribute("*", "*", "*");  // permite que el servicio web sea utilizado por cualquier cliente
+            config.EnableCors(cors);
+
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        // GET api/<controller>
+        // listar todos los usuarios
+        public List<Usuario> Get()
+        {
+            return UsuarioData.Listar();
+        }
+        // GET api/<controller>/5
+        // listar un usuario en particular
+        public Usuario Get(int id)
+        {
+            return UsuarioData.Obtener(id);
+        }
+        // POST api/<controller>
+        // registrar un usuario
+        public bool Post([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Registrar(oUsuario);
+        }
+        // PUT api/<controller>/5
+        // editar un usuario
+        public bool Put([FromBody] Usuario oUsuario)
+        {
+            return UsuarioData.Modificar(oUsuario);
+        }
+        // DELETE api/<controller>/5
+        // eliminar un usuario
+        public bool Delete(int id)
+        {
+            return UsuarioData.Eliminar(id);
+        }
+    }
+}
